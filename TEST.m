40504aaefc772395fd99f1fe360f8117479c708b
Timotej Stibilj <timotej.stibilj@gmail.com>
2023-07-03 22:54:04 +0200

    testna krivulja

---
+++ TEST.m
@@ -0,0 +1,11 @@
+% Bezierjevo krivulja b, b:[0,1]→ℝ^2, katere kontrolne točke so:
+% b = [-1 0 1;1 3 2]. 
+
+b = [-1 0 1;1 3 2]'; % podati moramo v dveh stolpcih, vsaka vrstica vsebuje
+% x in y koordinato ene kontrolne točke 
+
+plotBezier(b);
+
+
+
+
