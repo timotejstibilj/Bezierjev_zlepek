2b6c4c75f62e049c96d0a476912f7c9ff349c7b8
Timotej Stibilj <timotej.stibilj@gmail.com>
2023-07-07 10:26:17 +0200

    robni primer za samo 2 delilni tocki

--- tangente_kubicnega_C2_zlepka.m
+++ tangente_kubicnega_C2_zlepka.m
@@ -1,4 +1,4 @@
-function V =tangente_kubicnega_C2_zlepka(u,p,v0, vN)
+function V = tangente_kubicnega_C2_zlepka(u,p,v0,vN)
     % TANGENTE_KUBICNEGA_C2_ZLEPKA racuna tangentne vektorje v notranjosti C^2 kubičnega zlepka
     % V=TANGENTE_KUBICNEGA_C2_ZLEPKA(u,p) vrne matriko tangentnih vektorjev
     % v interpolacijskih točkah
@@ -9,22 +9,29 @@ function V =tangente_kubicnega_C2_zlepka(u,p,v0, vN)
     %TODO: zaenkrat je v R^2
     %dimenzija prostora:
     %d=size(u,1);
-    n = size(u, 1) - 2;
-
-    % razdalje med delilnimi točkami
-    du = u(2:end) - u(1:end-1);
-    a = du(3:end) ./ (du(3:end) + du(2:end-1));
-    c= du(1:end-2) ./ (du(1:end-2) + du(2:end-1));
-    b = 2*ones(n, 1);
-    %A = diag(a, -1) + diag(b, 0) + diag(c, 1);
+    n = size(u, 1) - 2; %stevilo notranjih delilnih tock
     
-    dp = p(2:end, 1:2) - p(1:end-1, 1:2);
-    F = 3 ./ (du(1:end-1) + du(2:end)) .* ( dp(2:end, 1:2) .* du(1:end-1) ./ du(2:end) + dp(1:end-1, 1:2) .* du(2:end) ./ du(1:end-1) );
-    % dodamo začetni in končni člen, ki ga dasta v0 in vN
-    F(1, 1:2) = F(1, 1:2) - v0 .* (du(2) / (du(2) + du(1)));
-    F(end, 1:2) = F(end, 1:2) - vN .* (du(end-1) / (du(end) + du(end-1)));
 
-    %notranji tangentni vektorjev v1, v2, ..., v_{N-1) C^2 kubičnega zlepka,
-    V_in = Thomas(a, b, c, F);
+    if n > 0 %v primeru dveh delilnih točk ni zlepka, ampak samo navadna Bezierjeva krivulja
+
+        % razdalje med delilnimi točkami
+        du = u(2:end) - u(1:end-1);
+        a = du(3:end) ./ (du(3:end) + du(2:end-1));
+        c= du(1:end-2) ./ (du(1:end-2) + du(2:end-1));
+        b = 2*ones(n, 1);
+        %A = diag(a, -1) + diag(b, 0) + diag(c, 1);
+        
+        dp = p(2:end, 1:2) - p(1:end-1, 1:2);
+        F = 3 ./ (du(1:end-1) + du(2:end)) .* ( dp(2:end, 1:2) .* du(1:end-1) ./ du(2:end) + dp(1:end-1, 1:2) .* du(2:end) ./ du(1:end-1) );
+        % dodamo začetni in končni člen, ki ga dasta v0 in vN
+        F(1, 1:2) = F(1, 1:2) - v0 .* (du(2) / (du(2) + du(1)));
+        F(end, 1:2) = F(end, 1:2) - vN .* (du(end-1) / (du(end) + du(end-1)));
+    
+        %notranji tangentni vektorjev v1, v2, ..., v_{N-1) C^2 kubičnega zlepka,
+        V_in = Thomas(a, b, c, F);
+    else
+        V_in = []; % za 2 delilnih točkah ni notranjih tangentnih vektorjev 
+    end
+    
     V = [v0; V_in; vN]; % celoten nabor vektorjev
 end
